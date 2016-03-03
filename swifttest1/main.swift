2260385c26ac4c2bc0461a7c248b257de73da32e
20131105753 <a20131105753@admindeiMac-35.local>
2016-03-03 11:09:01 +0800

    Initial Commit

---
+++ swifttest1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swifttest1
+//
+//  Created by 20131105753 on 16/3/3.
+//  Copyright (c) 2016年 20131105753. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
